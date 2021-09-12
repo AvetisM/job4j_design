476f261225ff63d8cd4beb3dc353dc16a8e3c8dc
AvetisM <avetis.mkhitaryants@gmail.com>
2021-09-12 13:34:54 +0300

    0. Что такое обобщенные типы (generics) [#4952]

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -7,12 +7,10 @@ import java.util.List;
 public class Generics {
     public static void main(String[] args) {
 
-        /*Generics gen = new Generics ();
-
-        List<Animal> first = new ArrayList<?extends Animal>();
+        Generics gen = new Generics();
+        List<Animal> first = new ArrayList<>();
         List<Predator> second = new ArrayList<>();
         List<Tiger> third = new ArrayList<>();
-
         first.add(new Animal());
         second.add(new Predator());
         third.add(new Tiger());
@@ -29,25 +27,25 @@ public class Generics {
 
         gen.printLowerBoundedWildCard(first);
         gen.printLowerBoundedWildCard(second);
-        gen.printLowerBoundedWildCard(third);*/
+        gen.printLowerBoundedWildCard(third);
     }
 
-    public void printObject(List<Object> list) {
-        for (Iterator<Object> it = list.iterator(); it.hasNext();) {
+    public void printObject(List<?> list) {
+        for (Iterator<?> it = list.iterator(); it.hasNext();) {
             Object next = it.next();
             System.out.println("Текущий элемент: " + next);
         }
     }
 
-    public void printBoundedWildCard(List<Predator> list) {
-        for (Iterator<Predator> it = list.iterator(); it.hasNext();) {
+    public void printBoundedWildCard(List<? extends Animal> list) {
+        for (Iterator<? extends Animal> it = list.iterator(); it.hasNext();) {
             Object next = it.next();
             System.out.println("Текущий элемент: " + next);
         }
     }
 
-    public void printLowerBoundedWildCard(List<Predator> list) {
-        for (Iterator<Predator> it = list.iterator(); it.hasNext();) {
+    public void printLowerBoundedWildCard(List<? super Tiger> list) {
+        for (Iterator<? super Tiger> it = list.iterator(); it.hasNext();) {
             Object next = it.next();
             System.out.println("Текущий элемент: " + next);
         }
