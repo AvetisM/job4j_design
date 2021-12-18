194da4ad3029f40862dd071ab649ca81760478ad
AvetisM <avetis.mkhitaryants@gmail.com>
2021-12-18 20:11:01 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("surname"), is("mkhitaryants"));
+        assertThat(config.value("name"), is("avetis"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("surname"), is("mkhitaryants"));
+        assertThat(config.value("name"), is("avetis"));
+    }
+
+    @Test
+    public void whenWrongPair() {
+        String path = "./data/pair_wrong_pair.properties";
+        Config config = new Config(path);
+        try {
+            config.load();
+        } catch (IllegalArgumentException ex) {
+            assertThat(ex.getMessage(), containsString("key or value is illegal"));
+        }
+    }
+}
