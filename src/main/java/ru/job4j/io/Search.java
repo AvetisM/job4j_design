e6b9ea649760a15bd08f2acd7cf5aa76498255db
AvetisM <avetis.mkhitaryants@gmail.com>
2022-01-02 22:28:35 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,11 +10,17 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Arguments not defined.");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Arguments are not defined.");
         }
-        if (args.length == 1) {
-            throw new IllegalArgumentException("Second parameter not defined.");
+        File newFile = new File(args[0]);
+        if (!newFile.exists()) {
+            throw new IllegalArgumentException(
+                    String.format("The path %s specified in the first parameter does not exist.", args[0]));
+        }
+        if (!newFile.isDirectory()) {
+            throw new IllegalArgumentException(
+                    String.format("The path %s specified in the first parameter is not a directory", args[0]));
         }
         Path start = Path.of(args[0]);
         String currentPredicate = args[1];
