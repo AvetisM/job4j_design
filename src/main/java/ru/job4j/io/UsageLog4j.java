ba8eb5f8bc34179c34552c57ace06a19e3d7749c
AvetisM <avetis.mkhitaryants@gmail.com>
2022-01-11 19:50:25 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,23 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Avetis Mkhitaryants";
+        int age = 34;
+        char grade = 'A';
+        float gradePrs = 79.9f;
+        double salary = 180.000d;
+        short group = 1123;
+        byte children = 2;
+        boolean married = true;
+
+        LOG.debug("User info name : {}, age : {}, grade : {}, gradePrs : {}, salary : {}, group : {}, married : {}, children : {}",
+                name,
+                age,
+                grade,
+                gradePrs,
+                salary,
+                group,
+                married,
+                children);
     }
 }
