6a7b87365fe931db005de5ddc02c234edb995101
AvetisM <avetis.mkhitaryants@gmail.com>
2022-01-13 21:06:58 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/java/Goods.java
@@ -0,0 +1,49 @@
+package ru.job4j.serialization.java;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+
+public class Goods {
+    private String name;
+    private double price;
+    private Boolean pack;
+    private String[] stock;
+    private Specifications specifications;
+
+    public Goods(String name, double price, Boolean pack, String[] stock,
+                 Specifications specifications) {
+        this.name = name;
+        this.price = price;
+        this.pack = pack;
+        this.stock = stock;
+        this.specifications = specifications;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{"
+                + "name='" + name + '\''
+                + ", price=" + price
+                + ", pack=" + pack
+                + ", stock=" + Arrays.toString(stock)
+                + ", specifications=" + specifications
+                + '}';
+    }
+
+    public static void main(String[] args) {
+
+       final Goods goods = new Goods("T-shirt", 450.00, false,
+                new String[]{"Moscow", "London"},
+                new Specifications("Grey", "M"));
+
+        final Gson gson = new GsonBuilder().create();
+        final String gsonGoods = gson.toJson(goods);
+
+        System.out.println(gsonGoods);
+
+        final Goods goods2 = gson.fromJson(gsonGoods, Goods.class);
+        System.out.println(goods2);
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/java/Specifications.java
@@ -0,0 +1,11 @@
+package ru.job4j.serialization.java;
+
+public class Specifications {
+    private String color;
+    private String size;
+
+    public Specifications(String color, String size) {
+        this.color = color;
+        this.size = size;
+    }
+}
