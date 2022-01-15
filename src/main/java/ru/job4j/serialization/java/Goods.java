e81f774118908a9897ff399370f59f8f992694e7
AvetisM <avetis.mkhitaryants@gmail.com>
2022-01-15 16:35:37 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/java/Goods.java
+++ src/main/java/ru/job4j/serialization/java/Goods.java
@@ -1,23 +1,37 @@
 package ru.job4j.serialization.java;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "goods")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Goods {
-    private final String name;
-    private final double price;
-    private final Boolean pack;
-    private final String[] stock;
-    private final Specifications specifications;
 
-    public Goods(String name, double price, Boolean pack, String[] stock,
+    @XmlAttribute
+    private String name;
+    @XmlAttribute
+    private double price;
+    @XmlAttribute
+    private Boolean pack;
+    private Specifications specifications;
+
+    @XmlElementWrapper(name = "warehouses")
+    @XmlElement(name = "warehouse")
+    private String[] warehouses;
+
+    public Goods() { }
+
+    public Goods(String name, double price, Boolean pack, String[] warehouses,
                  Specifications specifications) {
         this.name = name;
         this.price = price;
         this.pack = pack;
-        this.stock = stock;
+        this.warehouses = warehouses;
         this.specifications = specifications;
     }
 
@@ -27,23 +41,32 @@ public class Goods {
                 + "name='" + name + '\''
                 + ", price=" + price
                 + ", pack=" + pack
-                + ", stock=" + Arrays.toString(stock)
+                + ", warehouses=" + Arrays.toString(warehouses)
                 + ", specifications=" + specifications
                 + '}';
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
        final Goods goods = new Goods("T-shirt", 450.00, false,
                 new String[]{"Moscow", "London"},
                 new Specifications("Grey", "M"));
 
-        final Gson gson = new GsonBuilder().create();
-        final String gsonGoods = gson.toJson(goods);
+        JAXBContext context = JAXBContext.newInstance(Goods.class);
 
-        System.out.println(gsonGoods);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(goods, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
 
-        final Goods goods2 = gson.fromJson(gsonGoods, Goods.class);
-        System.out.println(goods2);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Goods result = (Goods) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 }
--- src/main/java/ru/job4j/serialization/java/Specifications.java
+++ src/main/java/ru/job4j/serialization/java/Specifications.java
@@ -1,8 +1,16 @@
 package ru.job4j.serialization.java;
 
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "specifications")
 public class Specifications {
+
+    @XmlAttribute
     private String color;
+    @XmlAttribute
     private String size;
+
+    public Specifications() { }
 
     public Specifications(String color, String size) {
         this.color = color;
