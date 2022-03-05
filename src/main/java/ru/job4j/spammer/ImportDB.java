050a62128abfa6bc5199dbcd15398b6c082b3d8f
AvetisM <avetis.mkhitaryants@gmail.com>
2022-03-05 20:39:57 +0300

    0.2. PrepareStatement [#379307 #269826]

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -19,7 +19,7 @@ public class ImportDB {
         this.dump = dump;
     }
 
-    public List<User> load() throws IOException {
+    public List<User> load() {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
             for (String line = rd.readLine(); line != null; line = rd.readLine()) {
@@ -29,6 +29,8 @@ public class ImportDB {
                     users.add(newUser);
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return users;
     }
